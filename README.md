d8f18358026551e102435093e9abacc8473ee09a
surasith.kae <surasith.kae@ascendcorp.com>
2024-04-05 22:58:55 +0700

    feat: init scripts to send email

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # email-sender
+
 PoC for send email using NodeJS
+
+## Setup
+
+- Install node packages with command `npm i`
+- Set the SMTP server, port, sender and receiver email in [config.json](/config.json)
+
+  ```json
+  {
+    "SMTPServer": "mailrelay.domain.go.th",
+    "SMTPPort": 587,
+    "SenderEmail": "sender@email.com",
+    "SenderPassword": "senderpassword",
+    "EnableSSL": false,
+    "ReceiverEmail": "receiver@email.com"
+  }
+  ```
+
+## Send email
+
+Run script to send email with command `node index.js`.
---
+++ index.js
@@ -0,0 +1,30 @@
+var config = require("./config.json");
+var nodemailer = require("nodemailer");
+
+var transporter = nodemailer.createTransport({
+  host: config.SMTPServer,
+  port: config.SMTPPort,
+  secure: config.EnableSSL,
+  auth: {
+    user: config.SenderEmail,
+    pass: config.SenderPassword,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
+
+var mailOptions = {
+  from: config.SenderEmail,
+  to: config.ReceiverEmail,
+  subject: "Test sending Email using Node.js",
+  html: "<h1>Welcome</h1><p>That was easy!</p>",
+};
+
+transporter.sendMail(mailOptions, function (error, info) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Email sent: " + info.response);
+  }
+});
